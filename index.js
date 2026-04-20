6d169877d51b0b6d8859cf1756101be3a76f4e26
NORA6228 <hukasigi15hai@gmail.com>
2026-04-20 09:17:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,50 @@ app.post('/webhook', async (req, res) => {
 
   const events = req.body.events || [];
   for (const event of events) {
- console.log("うんこ")
+    
+  // JSONデータのパース
+  var receiveJSON = JSON.parse(request.postData.contents);
+  var event = receiveJSON.events[0];
+  const input = event.message.text
+
+  // ユーザーIDの取得とチェック
+  var userId = event.source.userId;
+  var arrUser = Array.prototype.concat.apply([], UserData);
+  var i = arrUser.indexOf(userId);
+  var replyText;
+
+  //受信したものの分析
+  if (event.type === "message") {
+    console.log("1" + current);
+    console.log("2" + userId);
+    if (event.message.type === "image") {
+      //replyText = processImageMessage(event, userId);
+      console.log("うんこ")
+    } else {
+      if (input.match("ヘルプ")) {
+        //help(input);
+        text3 = "担当者にメッセージを送りました\n確認までしばらくお待ちください"
+        //replyToUser(event.replyToken, text3);
+        console.log("陳子" + text3)
+      }
+      else if (input.match("LINE ID確認メッセージ")) {
+        const eventData = JSON.parse(request.postData.contents).events[0];
+        const userId2 = eventData.source.userId;
+        const text2 = `あなたのUser_IDは${userId2}\nです。`;
+        console.log("4" + "LINE ID確認メッセージ");
+        //replyToUser(event.replyToken, text2);
+      }
+      else if (input.match("まえのしゃしんだして")) {
+        //getPublicUrl()
+        console.log("baka")
+      }
+    }
+
+    //sheet2.appendRow([current, userId, " ", replyText]); //ログ残すやつ（じかん、id、めっさげ、返信）
+
+  }
+
+
   }
 
   if (!GAS_URL) {
